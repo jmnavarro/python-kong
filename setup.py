da70553fe5437ac40827088d89525ea2dba83ca4
Dirk Moors <dirkmoors@gmail.com>
2015-07-27 16:12:45 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ requirements = [str(ir.req) for ir in parse_requirements('./requirements.txt', s
 
 setup(
     name='python-kong',
-    version='0.1.3',
+    version=__version__,
     license='BSD',
     description='A Python client for the Kong API (http://getkong.org/)',
     long_description='%s\n%s' % (read('README.rst'), re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))),
