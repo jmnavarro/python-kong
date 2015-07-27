5ddbd2ecf58057abbfc72d67bacc8b9fe4952439
Dirk Moors <dirkmoors@gmail.com>
2015-07-27 09:05:50 +0200

    Moved compatibility fixes to compat module, and perform some cleanup

--- src/kong/constants.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals, print_function
-
-try:
-    from http.client import OK, CREATED, CONFLICT, NO_CONTENT
-except ImportError:
-    # For python <= 2.6
-    from httplib import OK, CREATED, CONFLICT, NO_CONTENT
